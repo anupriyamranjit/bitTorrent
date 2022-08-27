8d11b99294519e9fd153117162b0955d88aa7b5b
Anupriyam Ranjit <anupriyamranjit@Anupriyams-MacBook-Air.local>
2022-08-27 11:10:49 -0400

    simple socket

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 'use strict'
-// Import FileSystem
-const fs = require('fs')
+// Imports
+const fs = require('fs');
+const bencode = require('bencode');
+const dgram = require('dgram');
+const Buffer = require('buffer').Buffer;
+const urlParse = require('url').parse;
+
 
 // Read File
-const torrent = fs.readFileSync('puppy.torrent')
+let torrent = fs.readFileSync('puppy.torrent')
 
-// Print content of File in utf8
-// Originally returns a buffer
-console.log(torrent.toString('utf8'))
+// Use bencode decoding
+torrent = bencode.decode(torrent)
+
+
+/*
+Print content of File in utf8
+
+Originally returns a buffer
+
+Announce property in object is the tracker URL
+ */
+
+const url = urlParse(torrent.announce.toString('utf8'));
+
+const socket = dgram.createSocket('udp4');
+
+const myMessage = Buffer.from("hello?", 'utf8');
+
+socket.send(myMessage, 0, myMessage.length, url.port, url.host, () => {});
+
+socket.on('message', msg => {
+    console.log('message is', msg);
+  });
+
+
 
 
 
